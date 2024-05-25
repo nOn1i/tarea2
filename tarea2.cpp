bca597dd76eb47c5f76fa9177a5832daa8408592
nOn1i <iamnotanybody1234@gmail.com>
2024-05-25 00:45:05 -0400

    ahora si

--- tarea2.cpp
+++ tarea2.cpp
@@ -1,1 +1,36 @@
 #include <iostream>
+using namespace std;
+
+class super_string {
+    private:
+        struct nodo{
+            nodo *left = nullptr, *right = nullptr;
+            int index;
+            char c;
+            nodo(int index, char c) {}
+            nodo() {}
+        };
+        int height = 0; //altura del arbol
+        int lenght = 0; //largo del super-string
+        nodo* root = nullptr; //raiz del super-string
+    public:
+        super_string() {}
+        void juntar(super_string &s);
+        void agregar(char c){
+            
+        }; //insertar un caracter en la ultima posicion
+        // en la izquierda esta el super_string a y en la derecha el super_string b
+        void separar(int i, super_string &a, super_string &b);
+        void reverso(){
+
+        };// no debe cambiar la altura del arbol
+        int recortar();// retorna this->height despues de recortar
+        string stringizar(); // debe ser O(n)
+        void limpiar(); // se deben borrar todos los nodos del super-string
+};
+
+
+int main(){
+    
+    return 0;
+}
